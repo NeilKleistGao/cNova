7638193584a7e992ecf1d3b490f310d6aabb22aa
Chin <41846794+choosingausernameisannoying@users.noreply.github.com>
2020-11-21 19:52:53 +0800

    Add files via upload

--- gramma/LR_first.h
+++ gramma/LR_first.h
@@ -1,70 +1,11 @@
 #include <stdc++.h>
 namespace gen_LR
 {
-    //Readable Stack
-    //Can fetch a segment of the stack
-    template <class T>
-    class ReadableStack
-    {
-        private:
-            std::vector<T> stack;
-        public:
-        void push(T element);
-        void pop();
-        //return -1 if no such element in the stack
-        //return the index of the first same element in the stack
-        int contain(T element);
-        bool empty() const{
-            return stack.empty();
-        }
-        std::vector<T> getRoute(int index);
-        std::vector<T> getRoute(T element);
-    };
-
-    template <class T>
-    void ReadableStack<T>::push(T element)
-    {
-        stack.push_back(element);
-    }
-    template <class T>
-    void ReadableStack<T>::pop()
-    {
-        stack.pop_back();
-    }
-    template <class T>
-    int ReadableStack<T>::contain(T element)
-    {
-        for(int i=0;i<stack.size();i++)
-        {
-            if(stack[i]==element)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    template <class T>
-    std::vector<T> ReadableStack<T>::getRoute(int index)
-    {
-        std::vector<T> route;
-        route.insert(route.end(),stack.begin()+index,stack.end());
-        return route;
-    }
-    template <class T>
-    std::vector<T> ReadableStack<T>::getRoute(T element)
-    {
-        int index=contain(element);
-        if(-1!=index)
-        {
-            return contain(index);
-        }
-        else
-        {
-            return std::vector<T> route;
-        }
-    }
-
     using Character_set=std::set<V_T>;
     std::unordered_map<P_left,Character_set,NodeHash> FIRST_set;//FITST set
     void generateFIRST();
+    void appendSet(gen_LR::Character_set& toBeAppended,gen_LR::Character_set& toAppend);
+    void cascadingUpdate(gen_LR::P_left updatedNode,
+                     std::unordered_map<gen_LR::P_left, gen_LR::Character_set, gen_LR::NodeHash> &influenceList,
+                     std::set<gen_LR::P_left> &updatedList);
 }
